2140ec41e7cec887f4c9717044c0c301aef8e5dc
Gabriel <gabrielrafael.grs@gmail.com>
2022-06-05 13:06:25 -0300

    encapsulamento

---
+++ Formacao Java/2 - Entendendo a orientação a objetos/EntendendoOO/src/quartaAula/Teste.java
@@ -0,0 +1,17 @@
+package quartaAula;
+
+public class Teste {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+
+        //conta.saldo não funciona mais
+        conta.deposita(100);
+        System.out.println(conta.getSaldo());
+
+        conta.setAgencia(1111);
+        conta.setNumero(11111);
+
+        conta.setTitular(new Cliente());
+        conta.getTitular().setNome("Gabriel");
+    }
+}
